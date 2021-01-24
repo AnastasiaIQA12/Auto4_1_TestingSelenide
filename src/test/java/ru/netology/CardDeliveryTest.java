45f574ae22052f8e78c927f6a125f4c372894185
AnastasiaIQA12 <Sinka8900@mail.ru>
2021-01-24 11:20:49 +0400

    Change Test

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,9 +1,13 @@
 package ru.netology;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
@@ -14,7 +18,13 @@ public class CardDeliveryTest {
     void shouldSubmitForm() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id=date] input").setValue("27.01.2021");
+        Date today=new Date();
+        SimpleDateFormat format=new SimpleDateFormat("dd.MM.yyyy");
+        Calendar c=Calendar.getInstance();
+        c.add(Calendar.DAY_OF_MONTH,4);
+        String todayPlusThree=(String)(format.format(c.getTime()));
+        $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"A"+Keys.DELETE);
+        $("[data-test-id=date] input").setValue(todayPlusThree);
         $("[data-test-id=name] input").setValue("Елифанова Анастасия");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
